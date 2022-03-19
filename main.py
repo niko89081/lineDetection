b531dc8ff9bd83e709cfc7d8688a9621c58cad63
niko89081 <90409933+niko89081@users.noreply.github.com>
2022-03-19 00:23:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 #https://stackoverflow.com/questions/45322630/how-to-detect-lines-in-opencv
 import cv2 as cv
 import numpy as np
-import math
 
-img = cv.imread("image6.jpeg")
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-blur = cv.blur(gray, (5,5))
-edges = cv.Canny(blur, 300, 450, 0, apertureSize=3)
-lines = cv.HoughLines(edges, 1, np.pi / 180, 150, None, 0, 0)
-if lines is not None:
-    for i in range(0, len(lines)):
-        rho = lines[i][0][0]
-        theta = lines[i][0][1]
-        a = math.cos(theta)
-        b = math.sin(theta)
-        x0 = a * rho
-        y0 = b * rho
-        pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
-        pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
-        cv.line(img, pt1, pt2, (100, 0, 69), 3, cv.LINE_AA)
-
-cv.imshow("HELLLOE", img)
+def averager(lines):
+        
+img = cv.imread("IMG_1696.jpg")
+scale_percent = 20 # percent of original size
+width = int(img.shape[1] * scale_percent / 100)
+height = int(img.shape[0] * scale_percent / 100)
+dim = (width, height)
+img = cv.resize(img, dim, interpolation= cv.INTER_AREA)
+grayed = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("imgmg", img)
 cv.waitKey(0)
-cv.destroyAllWindows()
+
+dst = cv.Canny(grayed, 575, 600)
+cv.imshow("dst", dst)
+blurred = cv.blur(dst, (3, 3))
+cv.imshow("blured lines", blurred)
+line1 = cv.HoughLinesP(blurred, 2, np.pi / 180, 150, None, 275, 0)
+print(len(line1))
+if line1 is not None:
+        for i in range(len(line1)):
+                l = line1[i][0]
+                print(line1)
+                cv.line(img, (l[0], l[1]), (l[2], l[3]), (0,255,0), 10, cv.LINE_AA)
+
+cv.imshow("img", img)
+cv.waitKey(0)
